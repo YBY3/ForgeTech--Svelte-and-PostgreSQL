6a70cf0df2a543a5971d4a934b996a9e2492573d
Analee <amaharaj@stedwards.edu>
2025-02-13 17:58:47 -0600

    Added tables for products and populated tables

--- project-55/apps/backend/flask_app/models.py
+++ project-55/apps/backend/flask_app/models.py
@@ -1,3 +1,4 @@
+#connecting model to the database
 from flask_app.extensions import db
 
 # Example User Model
@@ -8,3 +9,28 @@ class User(db.Model):
     
     def __repr__(self):
         return f'<User {self.username}>'
+    
+#Product Model, used the format from frontend provided to create the catalog and order functionalitites
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(550), nullable=False)
+    components = db.Column(db.ARRAY(db.String), nullable=False)
+    image = db.Column(db.String(120), nullable=False)
+
+    
+    def __repr__(self):
+        return f'<Product {self.name}>'
+    
+    #decided to add a to_dict file to easily convert to a JSON 
+    #used to connect to the frontend
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'components': self.components,
+            'image': self.image
+        }
