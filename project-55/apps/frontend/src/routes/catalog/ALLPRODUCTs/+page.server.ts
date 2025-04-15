69e0d9b34967052660bff6c6ca739a72696a4834
Austen Radigk <austenradigk@gmail.com>
2025-04-15 10:13:18 -0500

    Catalog Image FIX

--- project-55/apps/frontend/src/routes/catalog/ALLPRODUCTs/+page.server.ts
+++ project-55/apps/frontend/src/routes/catalog/ALLPRODUCTs/+page.server.ts
@@ -1,5 +1,5 @@
 import { fail, redirect } from '@sveltejs/kit';
-import { getFlaskURL } from '$lib/api';
+import { getFlaskURL, getImageURL } from '$lib/api';
 import type { RawProductType, ProductType } from '$lib/types/ProductTypes.js';
 
 
@@ -30,7 +30,7 @@ export const load = async ({ locals, fetch }) => {
             //Convert Image IDs to Image URLs
             productData = responseData.data.map((product: RawProductType) => ({
                 ...product,
-                image_urls: product.image_ids.map(id => `${getFlaskURL()}/api/images/${id}`)
+                image_urls: product.image_ids.map(id => `${getImageURL()}/api/images/${id}`)
             }));
         }
 
--- project-55/apps/frontend/src/routes/catalog/STORAGEs/+page.server.ts
+++ project-55/apps/frontend/src/routes/catalog/STORAGEs/+page.server.ts
@@ -1,5 +1,5 @@
 import { fail, redirect } from '@sveltejs/kit';
-import { getFlaskURL } from '$lib/api';
+import { getFlaskURL, getImageURL } from '$lib/api';
 import type { RawProductType, ProductType } from '$lib/types/ProductTypes.js';
 
 export const load = async ({ locals, fetch }) => {
@@ -30,7 +30,7 @@ export const load = async ({ locals, fetch }) => {
                 .filter((product: RawProductType) => product.product_type === "STORAGE")
                 .map((product: RawProductType) => ({
                     ...product,
-                    image_urls: product.image_ids.map(id => `${getFlaskURL()}/api/images/${id}`)
+                    image_urls: product.image_ids.map(id => `${getImageURL()}/api/images/${id}`)
                 }));
         }
 
